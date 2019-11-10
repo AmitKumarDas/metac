e6c273c13b68a4ff91fdb74e64809af406c4542f
Amit Kumar Das <amit.das@cloudbyte.com>
2019-11-10 18:36:29 +0530

    [example] add updated metac to clean uninstall openebs (#54)
    This commit updates this example to use the latest version of
    metac library. It has updated yamls, README and test.sh files.
    
    Signed-off-by: AmitKumarDas <amit.das@mayadata.io>

--- examples/gctl/uninstall-openebs/test.sh
+++ examples/gctl/uninstall-openebs/test.sh
@@ -9,11 +9,11 @@ cleanup() {
   echo "++ Clean up started"
   echo "--------------------------"
 
-  kubectl delete -f coolnerd.yaml || true
   kubectl delete -f operator.yaml || true
-  kubectl delete -f rbac.yaml || true
-  kubectl delete -f ns_and_crd.yaml || true
-  
+  kubectl delete cm --all -n uninstall-openebs || true
+  kubectl delete -f operator_rbac.yaml || true
+  kubectl delete -f operator_ns.yaml || true
+
   echo "--------------------------"
   echo "++ Clean up completed"
   echo "--------------------------"
@@ -24,34 +24,38 @@ trap cleanup EXIT
 #set -ex
 
 # my cr name
-my_res="cool-status-on-me"
-my_ns="set-status-on-cr"
+my_ns="uninstall-openebs"
+openebs_ns="openebs"
 
 echo -e "\n++ Installing operator"
-kubectl apply -f ns_and_crd.yaml
-kubectl apply -f rbac.yaml
+kubectl apply -f operator_ns.yaml
+kubectl apply -f operator_rbac.yaml
 kubectl apply -f operator.yaml
 echo -e "\n++ Installed operator successfully"
 
 
-echo -e "\n++ Applying custom resource that gets watched by metac - I"
-kubectl apply -f coolnerd.yaml
-echo -e "\n++ Applied custom resource successfully"
+echo -e "\n++ Applying openebs - I"
+kubectl apply -f openebs_ns.yaml
+kubectl apply -f openebs_sa_and_crds.yaml
+kubectl apply -f openebs_crs.yaml
+echo -e "\n++ Applied openebs successfully"
 
-echo -e "\n++Waiting for custom resource's status to be updated"
-until [[ "$(kubectl -n $my_ns get coolnerd $my_res -o 'jsonpath={.status.phase}')" == "Active" ]]; do sleep 1; done
-echo -e "\n++Custom resource's status updated successfully"
+echo -e "\n++Waiting for openebs namespace's finalizer to be updated"
+while [[ "$(kubectl get ns $openebs_ns -o 'jsonpath={.metadata.finalizers}')" == *protect.gctl.metac.openebs.io* ]]; do sleep 1; done
+echo -e "\n++Openebs' finalizer updated successfully"
 
-echo -e "\n++ Deleting custom resource that is watched by metac"
-kubectl delete -f coolnerd.yaml
-echo -e "\n++ Deleted custom resource successfully"
+echo -e "\n++ Deleting openebs namespace that is watched by metac"
+kubectl delete -f openebs_ns.yaml
+echo -e "\n++ Deleted openebs namespace successfully"
 
-echo -e "\n++ Applying custom resource that gets watched by metac - II"
-kubectl apply -f coolnerd.yaml
-echo -e "\n++ Applied custom resource successfully"
+echo -e "\n++ Verify openebs resources are not present"
+kubectl get ns
+kubectl get crd
+kubectl get runtasks -n openebs
+kubectl get castemplates
+kubectl get serviceaccounts
+kubectl get clusterroles
+kubectl get clusterrolebindings
+echo -e "\n++ Verified openebs resources are not present successfully"
 
-echo -e "\n++Waiting for custom resource's status to be updated"
-until [[ "$(kubectl -n $my_ns get coolnerd $my_res -o 'jsonpath={.status.phase}')" == "Active" ]]; do sleep 1; done
-echo -e "\n++Custom resource's status updated successfully"
-
-echo -e "\n++ Test set-status-on-cr completed successfully"
+echo -e "\n++ Test uninstall-openebs completed successfully"
