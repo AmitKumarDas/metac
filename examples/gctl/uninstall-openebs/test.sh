18331ce25f476fe15db504a6e6658bd0942df858
AmitKumarDas <amit.das@mayadata.io>
2019-11-12 14:58:54 +0530

    [example] update steps to automate uninstall openebs
    This commit updates test.sh that automates verification of
    uninstalling openebs
    
    Signed-off-by: AmitKumarDas <amit.das@mayadata.io>

--- examples/gctl/uninstall-openebs/test.sh
+++ examples/gctl/uninstall-openebs/test.sh
@@ -26,6 +26,7 @@ trap cleanup EXIT
 # my cr name
 my_ns="uninstall-openebs"
 openebs_ns="openebs"
+finalizer="protect.gctl.metac.openebs.io/uninstall-openebs"
 
 echo -e "\n++ Installing operator"
 kubectl apply -f operator_ns.yaml
@@ -35,14 +36,22 @@ kubectl apply -f operator.yaml
 echo -e "\n++ Installed operator successfully"
 
 
-echo -e "\n++ Applying openebs - I"
+echo -e "\n++ Applying openebs"
 kubectl apply -f openebs_ns.yaml
 kubectl apply -f openebs_sa_and_crds.yaml
 kubectl apply -f openebs_crs.yaml
 echo -e "\n++ Applied openebs successfully"
 
+echo -e "\n++ Verify openebs resources are present"
+kubectl get serviceaccounts -n $openebs_ns --no-headers 2>/dev/null | wc -l && echo "\__serviceaccounts"
+kubectl get clusterroles --no-headers | grep $openebs_ns | wc -l && echo "\__clusterroles"
+kubectl get clusterrolebindings --no-headers | grep $openebs_ns | wc -l  && echo "\__clusterrolebindings"
+kubectl get ns $openebs_ns --no-headers 2>/dev/null | wc -l && echo "\__namespaces"
+kubectl get crd --no-headers | grep $openebs_ns | wc -l && echo "\__crds"
+echo -e "\n++ Verified openebs resources are present successfully"
+
 echo -e "\n++Waiting for openebs namespace's finalizer to be updated"
-while [[ "$(kubectl get ns $openebs_ns -o 'jsonpath={.metadata.finalizers}')" == *protect.gctl.metac.openebs.io* ]]; do sleep 1; done
+until [[ "$(kubectl get ns $openebs_ns -o 'jsonpath={.metadata.finalizers}')" == "[${finalizer}]" ]]; do sleep 1; done
 echo -e "\n++Openebs' finalizer updated successfully"
 
 echo -e "\n++ Deleting openebs namespace that is watched by metac"
@@ -50,13 +59,11 @@ kubectl delete -f openebs_ns.yaml
 echo -e "\n++ Deleted openebs namespace successfully"
 
 echo -e "\n++ Verify openebs resources are not present"
-kubectl get ns
-kubectl get crd
-kubectl get runtasks -n openebs
-kubectl get castemplates
-kubectl get serviceaccounts
-kubectl get clusterroles
-kubectl get clusterrolebindings
+kubectl get serviceaccounts -n $openebs_ns --no-headers 2>/dev/null | wc -l && echo "\__serviceaccounts"
+kubectl get clusterroles --no-headers | grep $openebs_ns | wc -l && echo "\__clusterroles"
+kubectl get clusterrolebindings --no-headers | grep $openebs_ns | wc -l  && echo "\__clusterrolebindings"
+kubectl get ns $openebs_ns --no-headers 2>/dev/null | wc -l && echo "\__namespaces"
+kubectl get crd --no-headers | grep $openebs_ns | wc -l && echo "\__crds"
 echo -e "\n++ Verified openebs resources are not present successfully"
 
 echo -e "\n++ Test uninstall-openebs completed successfully"
