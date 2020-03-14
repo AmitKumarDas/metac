fc85a5672a489ef704b1aed3ae3dc22e3a353195
AmitKumarDas <amit.das@mayadata.io>
2020-03-14 13:31:38 +0530

    feat(advanced selector): add refkey to match attachment against watch
    This commit adds a new field in advanced selector to match an
    attachment against a watch with target's nested field path
    different than watch's nested field path.
    
    For example, if attachment's spec.watchname is same as the
    watch's metadata.name then this attachment can be filtered
    (i.e. a successful match) via advanced selector.
    
    ```yaml
    matchReferenceExpressions:
    - key: spec.watchname # attachment
      refKey: metadata.name # watch
    ```
    
    In addition, this change understands field names with dot '.'
    characters.
    
    NOTE: One uses advanced selector to filter attachments given
    a watch based on watch's uid, name, namespace, labels,
    annotations, spec, status, etc. fields.
    
    NOTE: Advanced selector logic i.e. code that deals with
    MatchReference & MatchReferenceExpressions are refactor-ed to
    a separate reference.go file.
    
    Signed-off-by: AmitKumarDas <amit.das@mayadata.io>

--- apis/metacontroller/v1alpha1/selector.go
+++ apis/metacontroller/v1alpha1/selector.go
@@ -30,17 +30,34 @@ type ResourceSelector struct {
 // The result of match(-es) are ANDed.
 type SelectorTerm struct {
 	// MatchSlice is a map i.e. key value pairs based slice selector.
-	// A single {key,value} in the MatchSlice map is equivalent to
-	// an element of matchSliceExpressions, whose key field is "key",
-	// the operator is "In", and the value contains array of string
-	// values.
 	//
-	// A key should represent the nested field path of the target under
-	// match separated by dot(s) i.e. '.'
+	// A single {key,value} pair in the MatchSlice map is equivalent
+	// to one element in MatchSliceExpressions.
+	//
+	// NOTE:
+	// 	A MatchFields is internally converted to MatchFieldExpressions
+	//
+	// For example following matches are same:
+	//
+	//  matchSlice:
+	//    metadata.finalizers: ["protect-101", "protect-102"]
+	//
+	//  matchSliceExpressions:
+	//  - key: metadata.finalizers
+	//    operator: In
+	//    values:
+	//    - protect-101
+	//    - protect-102
+	//
+	// A key should represent the nested field path separated by dot(s)
+	// e.g. 'spec.items'
+	//
+	// NOTE:
+	//	Values at these field paths should be of **[]string** type.
 	//
 	// A MatchSlice is converted into a list of SliceSelectorRequirement
-	// that are AND-ed to determine if the selector matches its target or
-	// not.
+	// that are AND-ed to determine if the selector matches its **target**
+	// or not.
 	//
 	// This is optional
 	MatchSlice map[string][]string `json:"matchSlice"`
@@ -49,19 +66,36 @@ type SelectorTerm struct {
 	// These requirements are AND-ed to determine if the selector matches
 	// its target or not.
 	//
-	// The slice selector requirement key should represent the nested
-	// field path of the target under match separated by dot(s) i.e. '.'
-	//
 	// This is optional
 	MatchSliceExpressions []SliceSelectorRequirement `json:"matchSliceExpressions"`
 
 	// MatchFields is a map i.e. key value pairs based field selector.
-	// A single {key,value} in the MatchFields map is equivalent to an
-	// element of matchFieldExpressions, whose key field is "key", the
-	// operator is "In", and the value contains only a string value.
 	//
-	// A key should represent the nested field path of the target under
-	// match separated by dot(s) i.e. '.'
+	// A single {key, value} pair in the MatchFields map is equivalent
+	// to one element in MatchFieldExpressions.
+	//
+	// NOTE:
+	// 	A MatchFields is internally converted to MatchFieldExpressions
+	//
+	// For example following matches are same:
+	//
+	// 	matchFields:
+	//    metadata.uid: "uid-101"
+	//    metadata.name: "abc"
+	//
+	//  matchFieldExpressions:
+	//  - key: metadata.uid
+	//    operator: In
+	//    values: ["uid-101"]
+	//  - key: metadata.name
+	//    operator: In
+	//    values: ["abc"]
+	//
+	// A key should represent the nested field path separated by dot(s)
+	// e.g. 'status.phase'
+	//
+	// NOTE:
+	//	Values at these field paths should be of **string** type.
 	//
 	// A MatchFields is converted into a list of LabelSelectorRequirement
 	// that are AND-ed to determine if the selector matches its target or
@@ -73,47 +107,85 @@ type SelectorTerm struct {
 	// MatchFieldExpressions is a list of field selector requirements.
 	// The requirements are AND-ed.
 	//
-	// The label selector requirement key should represent the nested
-	// field path of the target under match separated by dot(s) i.e. '.'
-	//
 	// This is optional
 	MatchFieldExpressions []metav1.LabelSelectorRequirement `json:"matchFieldExpressions"`
 
-	// MatchReference is a list of keys where each key holds the path to a
-	// field present in target resource as well as the reference resource.
-	// A single key in the MatchReference list is equivalent to an
-	// element of MatchReferenceExpressions, whose key field is "key", and
-	// the operator is "Equals".
+	// MatchReference is a list of keys where each key holds the
+	// path to a nested field present in both target resource as
+	// well as the reference resource.
 	//
-	// A key should represent the nested field path of the target as well
-	// watch separated by dot(s) e.g. 'metadata.name'
+	// NOTE:
+	//	A target is as an attachment resource whereas a reference
+	// is the watch resource when used in the context of
+	// MetaController.
+	//
+	// A single item in the MatchReference list is equivalent to one
+	// element in MatchReferenceExpressions.
+	//
+	// NOTE:
+	// 	A MatchReference is internally converted to
+	// MatchReferenceExpressions.
+	//
+	// For example following matches are same:
+	//
+	// 	matchReference: ["metadata.uid", "metadata.name"]
+	//
+	//  matchReferenceExpressions:
+	//  - key: metadata.uid
+	//    operator: Equals
+	//  - key: metadata.name
+	//    operator: Equals
+	//
+	// A key should represent the nested field path separated by dot(s)
+	// e.g. 'status.phase'
+	//
+	// NOTE:
+	//	Values at these field paths should be of **string** type.
 	//
 	// A MatchReference is converted into a list of LabelSelectorRequirement
-	// that are AND-ed to determine if the selector marks its target as
-	// a match or no match.
+	// that are AND-ed to determine if the selector marks its target _(read
+	// attachment)_ as a match or no match.
+	//
+	// NOTE:
+	//	This tries to match the target _(i.e. attachment object)_ based on
+	// reference _(i.e. watch object)_. A match is successful if values
+	// extracted from these objects match.
 	//
 	// This is optional
-	MatchReference []string `json:"matchWatch"`
+	MatchReference []string `json:"matchReference"`
 
 	// MatchReferenceExpressions is a list of field selector requirements.
 	// The requirements are AND-ed.
 	//
-	// A label selector requirement key should represent the nested
-	// field path of the target separated by dot(s) e.g. 'metadata.uid'
-	//
-	// This result of each item in this list of LabelSelectorRequirements
-	// is AND-ed to determine if the selector marks its target as
-	// a match or no match.
-	//
 	// This is optional
-	MatchReferenceExpressions []ReferenceSelectorRequirement `json:"matchWatchExpressions"`
+	MatchReferenceExpressions []ReferenceSelectorRequirement `json:"matchReferenceExpressions"`
 
-	// MatchLabels is a map of {key,value} pairs. A single {key,value}
-	// in the MatchLabels map is equivalent to an element of
-	// MatchLabelExpressions, whose key field is "key", the operator is "In",
-	// and the value contains a string value. The requirements are AND-ed.
+	// MatchLabels is a map of {key,value} pairs that is matched against
+	// the target's labels.
 	//
-	// The key as well value is matched against the target's labels.
+	// A single {key, value} pair in the MatchLabels map is equivalent
+	// to one element in MatchLabelExpressions.
+	//
+	// NOTE:
+	// 	A MatchLabels is internally converted to MatchLabelExpressions
+	//
+	// For example following matches are same:
+	//
+	//  matchLabels:
+	//    app: metac
+	//
+	//  matchLabelExpressions:
+	//  - key: app
+	//    operator: In
+	//    values: ["metac"]
+	//
+	// MatchLabels is converted into a list of LabelSelectorRequirement
+	// that are AND-ed to determine if the selector matches its target or
+	// not.
+	//
+	// NOTE:
+	// 	Presence of key as well value in the target's **labels** is
+	// considered as a successful match.
 	//
 	// This is optional
 	MatchLabels map[string]string `json:"matchLabels"`
@@ -121,18 +193,35 @@ type SelectorTerm struct {
 	// MatchLabelExpressions is a list of label selector requirements.
 	// The requirements are ANDed.
 	//
-	// The label selector requirement's key as well value is matched
-	// against the target's labels.
-	//
 	// This is optional
 	MatchLabelExpressions []metav1.LabelSelectorRequirement `json:"matchLabelExpressions"`
 
-	// MatchAnnotations is a map of {key,value} pairs. A single {key,value}
-	// in the MatchAnnotations map is equivalent to an element of
-	// MatchAnnotationExpressions, whose key field is "key", the operator is
-	// "In", and the value contains a string value. The requirements are ANDed.
+	// MatchAnnotations is a map of {key,value} pairs that is matched against
+	// the target's annotations.
 	//
-	// The key as well value is matched against the target's annotations.
+	// A single {key, value} pair in the MatchAnnotations map is equivalent
+	// to one element in MatchAnnotationExpressions.
+	//
+	// NOTE:
+	// 	A MatchAnnotations is internally converted to MatchAnnotationExpressions
+	//
+	// For example following matches are same:
+	//
+	// 	matchAnnotations:
+	//    app: metac
+	//
+	//  matchAnnotationExpressions:
+	//  - key: app
+	//    operator: In
+	//    values: ["metac"]
+	//
+	// MatchAnnotations is converted into a list of LabelSelectorRequirement
+	// that are AND-ed to determine if the selector matches its target or
+	// not.
+	//
+	// NOTE:
+	// 	Presence of key as well value in the target's **annotations** is
+	// considered as a successful match.
 	//
 	// This is optional
 	MatchAnnotations map[string]string `json:"matchAnnotations"`
@@ -194,11 +283,35 @@ const (
 // reference resource refers to the parent / watch resource in
 // various meta controllers.
 type ReferenceSelectorRequirement struct {
-	// Key is the target's nested path that the selector applies to.
-	// The nested path is separated by dot(s) e.g. 'metadata.namespace'
+	// Key is the **target**'s nested path that the selector
+	// applies against. The nested path is separated by dot(s).
+	// E.g. 'metadata.namespace', 'metadata.name', 'status.phase',
+	// etc.
+	//
+	// NOTE:
+	//	A target object refers to an attachment in MetaController's
+	// terminology
 	Key string `json:"key"`
 
-	// Operator represents the key's relationship to a string value
+	// RefKey is the **reference**'s nested path that the selector
+	// applies against. This field is optional.
+	//
+	// NOTE:
+	//	A reference object refers to a watch in MetaController's
+	// terminology
+	//
+	// NOTE:
+	//	When set, the Operator field becomes optional since Operator
+	// is set to Equals.
+	RefKey string `json:"refKey"`
+
+	// Operator represents the operation that will be undertaken
+	// between the values extracted from target & reference. Both
+	// these values will be found at respective path declared in
+	// the key.
+	//
+	// NOTE:
+	//	Value at these field paths should be of string type.
 	Operator ReferenceSelectorOperator `json:"operator"`
 }
 
